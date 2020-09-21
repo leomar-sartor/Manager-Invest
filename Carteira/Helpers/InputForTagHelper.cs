85a8191d3bafe256a031fdb1bc3a6828b367935b
Leomar Sartor <leomar_sartor@unochapeco.edu.br>
2020-09-21 19:30:48 -0300

    Upgrade
    Upgrade

--- Carteira/Helpers/InputForTagHelper.cs
+++ Carteira/Helpers/InputForTagHelper.cs
@@ -1,5 +1,4 @@
-﻿using Carteira.Domain.Infra;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
